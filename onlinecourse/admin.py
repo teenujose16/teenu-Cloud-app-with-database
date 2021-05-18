7dcb3712c4a3d1d2a963c8b2e4c272112f08f21a
teenujose16 <teenujose16@gmail.com>
2021-05-18 01:18:29 +0000

    modifed admin file

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -9,6 +9,13 @@ class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+class QuestionInLine(admin.StackedInline):
+    model = Question
+    extra = 5
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 4
+
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -21,6 +28,10 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [QuestionInLine]
+class ChoiceAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
 
 # <HINT> Register Question and Choice models here
 
@@ -28,3 +39,6 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice,ChoiceAdmin
