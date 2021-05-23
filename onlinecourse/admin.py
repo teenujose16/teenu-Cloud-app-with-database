e356b08266700c952de34072535c98661de4f970
teenujose16 <teenujose16@gmail.com>
2021-05-23 06:38:49 +0000

    changes in admin.on in admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -5,9 +5,7 @@ from .models import Course, Lesson, Instructor, Learner, Question, Choice
 # <HINT> Register QuestionInline and ChoiceInline classes here
 class ChoiceInline(admin.StackedInline):
     model = Choice
-    extra = 5
-    class QuestionInline(admin.StackedInline):
-    model = Question
+    extra = 4
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
