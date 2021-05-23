6fa244c2aece5008fe13221eb8e112bbbee90b0e
teenujose16 <teenujose16@gmail.com>
2021-05-23 06:33:22 +0000

    some modification in admin.on in admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 5
+    class QuestionInline(admin.StackedInline):
+    model = Question
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
